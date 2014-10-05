e7b29d8ef397525122b7a576050b5af5eba37c0d
David McFadzean <davidmc@gmail.com>
2014-10-05 12:10:11 -0400

    Added meta template

--- routes/meta.js
+++ routes/meta.js
@@ -1,35 +1,34 @@
 var router = require("express").Router(),
-    renderer = require('../lib/renderer'),
-    fs = require("fs"),
-    models = require("../lib/models");
+renderer = require('../lib/renderer'),
+fs = require("fs"),
+models = require("../lib/models");
 
 models.use(Git);
+
+var files = [];
+
+function initFiles() {
+  Git.ls("*", function(err, list) {
+
+    console.log("\n\n>>>meta " + list.length + "\n\n");
+
+    for(var i = 0; i < list.length; i++) {
+      files.push(list[i]);
+      console.log(i, list[i]);
+    } 
+  });
+}
+
+initFiles();
 
 router.get("/meta", _getMeta);
 
 function _getMeta(req, res) {
 
-  var page = "#meta\n";
-
-  Git.ls("*", function(err, list) {
-
-      console.log("\n\n>>>meta " + list.length + "\n\n");
-
-     page += list.length + " files found\n";
-
-      for(var i = 0; i < list.length; i++) {
-          var file = list[i];
-	  console.log(i, file);
-	  page += "* [" + file + "](/meta/" + file + ")\n";
-      } 
-
-    console.log(page);
-
-    res.render("minimal", {
-      title: "meta",
-      content: renderer.render(page)
-    });
- });
+  res.render("meta", {
+    title: "meta",
+    files: files
+  });
 }
 
 module.exports = router;
