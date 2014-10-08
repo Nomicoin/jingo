c6fde54a2000fbb78f87728498827af1d75a898f
David McFadzean <davidmc@gmail.com>
2014-10-07 20:28:07 -0400

    Generates metadata file path

--- scripts/initmeta.py
+++ scripts/initmeta.py
@@ -1,28 +1,47 @@
 from pygit2 import *
 import uuid
 
+class File:
+    def __init__(self, entry):
+        self.xid = uuid.uuid4()
+        self.entry = entry
+
 class Snapshot:
     def __init__(self, commit):
         self.commit = commit
-        self.files = []
-        for entry in commit.tree:
-            self.files.append(entry)
+        self.files = {}
+        self.xids = {}
+
+    def add(self, entry, xid):
+        metadata = str(xid)[:8] + "/" + str(entry.id)[:8]
+        self.files[entry.name] = metadata
+        self.xids[entry.hex] = metadata
 
 class Project:
     def __init__(self):
         self.xid = uuid.uuid4()
+        self.snapshots = []
+        self.files = {}
     
     def init(self, repoDir):
         self.repo = Repository(repoDir)
-        self.snapshots = []
         for commit in self.repo.walk(self.repo.head.target, GIT_SORT_TIME | GIT_SORT_REVERSE):
-            self.snapshots.append(Snapshot(commit))
+            snapshot = Snapshot(commit)
+            for entry in commit.tree:
+                if entry.name in self.files:
+                    file = self.files[entry.name]
+                else:
+                    file = File(entry)
+                    self.files[entry.name] = file
+                    print "adding", entry.name
+                snapshot.add(entry, file.xid)
+            self.snapshots.append(snapshot)
 
 project = Project()
 project.init('/home/david/dev/Meridion.wiki/.git')
 
-for snapshot in project.snapshots:
-    print snapshot.commit.id, len(snapshot.files)
+#for snapshot in project.snapshots:
+#    print snapshot.commit.id, len(snapshot.files)
 
 print len(project.snapshots)
 
