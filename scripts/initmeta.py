ba0fc7b19fece189ceea24926125d56c81cb64e8
David McFadzean <davidmc@gmail.com>
2014-10-08 20:04:18 -0400

    Generates metadata paths

--- scripts/initmeta.py
+++ scripts/initmeta.py
@@ -13,9 +13,9 @@ class Snapshot:
         self.xids = {}
 
     def add(self, entry, xid):
-        metadata = str(xid)[:8] + "|" + str(entry.id)[:8]
-        self.files[entry.name] = metadata
-        self.xids[entry.hex] = str(xid)
+        #metadata = str(xid)[:8] + "|" + str(entry.id)[:8]
+        #self.files[entry.name] = metadata
+        self.xids[entry.hex] = [str(xid), entry.name]
 
 class Project:
     def __init__(self):
@@ -37,6 +37,16 @@ class Project:
                 snapshot.add(entry, file.xid)
             self.snapshots.append(snapshot)
 
+def createPath(xid, hash):
+    xidRef = str(xid)[:8]
+    hashRef = str(hash)[:8]
+    dirName = os.path.join(".meta", xidRef)
+    metadata = hashRef + ".toml"
+    path = os.path.join(dirName, metadata)
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+    return path
+
 project = Project()
 project.init('/home/david/dev/Meridion.wiki/.git')
 
@@ -46,15 +56,13 @@ print len(project.snapshots)
 #print toml.dumps(latest.xids)
 
 for snapshot in project.snapshots:
-    metadata = str(project.xid)[:8] + "|" + str(snapshot.commit.id)[:8]
-    path = os.path.join(".meta", *metadata.split("|")) + ".toml"
-    print metadata, path, len(snapshot.xids)
-
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
-
+    path = createPath(project.xid, snapshot.commit.id)
     with open(path, 'w') as f:
         f.write(toml.dumps(snapshot.xids))
+    for hash in snapshot.xids:
+        xid, name = snapshot.xids[hash]
+        path = createPath(xid, hash)
+        print path, name
 
 save = { 'project': { 'repo': project.repo.path, 'xid': str(project.xid) } }
 
