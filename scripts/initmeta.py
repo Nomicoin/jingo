cd044f29e7058ad21f2eebac787faac8c06bc249
David McFadzean <davidmc@gmail.com>
2014-10-11 13:12:46 -0400

    Project now initializes from saved state

--- scripts/initmeta.py
+++ scripts/initmeta.py
@@ -2,9 +2,9 @@ import uuid, toml, os, shutil
 from pygit2 import *
 from datetime import datetime
 
-class File:
-    def __init__(self, entry):
-        self.xid = uuid.uuid4()
+class Asset:
+    def __init__(self, entry, xid):
+        self.xid = xid
         self.entry = entry
 
 class Snapshot:
@@ -16,24 +16,110 @@ class Snapshot:
         self.xids[entry.hex] = [str(xid), entry.name]
 
 class Project:
-    def __init__(self):
-        self.xid = uuid.uuid4()
-        self.snapshots = []
-        self.files = {}
-    
-    def init(self, repoDir):
+    def __init__(self, repoDir):
         self.repo = Repository(repoDir)
+        self.snapshots = []
+        self.assets = {}
+        self.path = os.path.join(".meta", "index.toml")
+ 
+    def open(self):
+        if os.path.exists(self.path):
+            with open(self.path) as f:
+                projects = toml.loads(f.read())['projects']
+                if self.repo.path in projects:
+                    self.xid = projects[self.repo.path]
+                else:
+                    self.save()
+        else:
+            self.save()
+
+    def save(self):
+            self.xid = str(uuid.uuid4())
+            save = { 'projects': { self.repo.path: str(self.xid) } }
+            
+            metaDir = os.path.dirname(self.path)
+            if not os.path.exists(metaDir):
+                os.makedirs(metaDir)
+
+            with open(self.path, 'w') as f:
+                f.write(toml.dumps(save))
+
+    def init(self):
+        self.initSnapshots()
+        self.initMetadata()
+
+    def initSnapshots(self):
         for commit in self.repo.walk(self.repo.head.target, GIT_SORT_TIME | GIT_SORT_REVERSE):
             snapshot = Snapshot(commit)
-            for entry in commit.tree:
-                if entry.name in self.files:
-                    file = self.files[entry.name]
-                else:
-                    file = File(entry)
-                    self.files[entry.name] = file
-                    print "adding", entry.name
-                snapshot.add(entry, file.xid)
+            path, link = createPath(self.xid, commit.id)
+            if os.path.exists(path):
+                with open(path) as f:
+                    assets = toml.loads(f.read())['assets']
+                for id in assets:
+                    xid, name = assets[id]
+                    entry = commit.tree[name]
+                    if name in self.assets:
+                        asset = self.assets[entry.name]
+                    else:
+                        print "Adding %s as %s" % (name, xid)
+                        self.assets[name] = Asset(entry, xid)
+                    snapshot.add(entry, xid)
+            else:
+                for entry in commit.tree:
+                    if entry.name in self.assets:
+                        asset = self.assets[entry.name]
+                    else:
+                        asset = Asset(entry, uuid.uuid4())
+                        self.assets[entry.name] = asset
+                        print "Adding", entry.name
+                    snapshot.add(entry, asset.xid)
+
+                path, commitLink  = createPath(project.xid, snapshot.commit.id)
+                dt = datetime.fromtimestamp(snapshot.commit.commit_time)
+                metaCommit = {
+                    'project': str(project.xid),
+                    'author': snapshot.commit.author.name,
+                    'email': snapshot.commit.author.email,
+                    'time': dt.isoformat(),
+                    'message': snapshot.commit.message,
+                    'commit': str(snapshot.commit.id)
+                }
+
+                with open(path, 'w') as f:
+                    f.write(toml.dumps({'commit': metaCommit, 'assets': snapshot.xids}))
+
             self.snapshots.append(snapshot)
+                        
+    def initMetadata(self):
+        for snapshot in self.snapshots:
+            path, commitLink  = createPath(self.xid, snapshot.commit.id)
+            dt = datetime.fromtimestamp(snapshot.commit.commit_time)
+
+            for hash in snapshot.xids:
+                blob = self.repo[hash]
+                if blob.type != GIT_OBJ_BLOB:
+                    continue
+                xid, name = snapshot.xids[hash]
+                path, blobLink = createPath(xid, hash)
+                if not os.path.isfile(path):
+                    meta = {
+                        'xid': str(xid),
+                        'commit': commitLink,
+                        'type': '',
+                        'link': blobLink,
+                        'name': name,
+                        'description': '',
+                        'authors': str([]),
+                        'timestamp': dt.isoformat(),
+                        'asset': hash, 
+                        'size': blob.size,
+                        'encoding': 'binary' if blob.is_binary else 'text',
+                        'comments': '',
+                        'votes': ''
+                    }
+                    with open(path, 'w') as f:
+                        f.write(toml.dumps({'xidb': meta}))
+                        print "wrote metadata for", name
 
 def createPath(xid, hash):
     xidRef = str(xid)[:8]
@@ -46,56 +132,11 @@ def createPath(xid, hash):
         os.makedirs(dirName)
     return path, pathId
 
-project = Project()
-project.init('/home/david/dev/Meridion.wiki/.git')
+project = Project('/home/david/dev/Meridion.wiki/.git')
+project.open()
+project.init()
 
 print len(project.snapshots)
 
-if os.path.exists('.meta'):
-    shutil.rmtree('.meta')
 
-for snapshot in project.snapshots:
-    path, commitLink  = createPath(project.xid, snapshot.commit.id)
-    dt = datetime.fromtimestamp(snapshot.commit.commit_time)
-    metaCommit = {
-        'project': str(project.xid),
-        'author': snapshot.commit.author.name,
-        'email': snapshot.commit.author.email,
-        'time': dt.isoformat(),
-        'message': snapshot.commit.message,
-        'commit': str(snapshot.commit.id)
-    }
 
-    with open(path, 'w') as f:
-        f.write(toml.dumps({'commit': metaCommit, 'assets': snapshot.xids}))
-
-    for hash in snapshot.xids:
-        blob = project.repo[hash]
-        if blob.type != GIT_OBJ_BLOB:
-            continue
-        xid, name = snapshot.xids[hash]
-        path, blobLink = createPath(xid, hash)
-        if not os.path.isfile(path):
-            meta = {
-                'xid': str(xid),
-                'commit': commitLink,
-                'type': '',
-                'link': blobLink,
-                'name': name,
-                'description': '',
-                'authors': str([]),
-                'timestamp': dt.isoformat(),
-                'asset': hash, 
-                'size': blob.size,
-                'encoding': 'binary' if blob.is_binary else 'text',
-                'comments': '',
-                'votes': ''
-            }
-            print meta
-            with open(path, 'w') as f:
-                f.write(toml.dumps({'xidb': meta}))
-
-save = { 'project': { 'repo': project.repo.path, 'xid': str(project.xid) } }
-
-with open(os.path.join(".meta", "index.toml"), 'w') as f:
-    f.write(toml.dumps(save))
