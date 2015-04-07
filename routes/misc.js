d5d45c9ba0414bd787b108f6feb85dc750013180
David McFadzean <davidmc@gmail.com>
2015-04-07 16:19:40 -0400

    Removed deprecated jingo code

--- routes/misc.js
+++ routes/misc.js
@@ -1,13 +1,9 @@
 var router = require("express").Router(),
     renderer = require('../lib/renderer'),
-    fs = require("fs"),
-    models = require("../lib/models");
-
-models.use(Git);
+    fs = require("fs");
 
 router.get("/misc/syntax-reference", _getSyntaxReference);
 router.post("/misc/preview",         _postPreview);
-router.get("/misc/existence",        _getExistence);
 
 function _getSyntaxReference(req, res) {
   res.render('syntax');
@@ -16,30 +12,6 @@ function _getSyntaxReference(req, res) {
 function _postPreview(req, res) {
   res.render('preview', {
     content: renderer.render(req.body.data)
-  });
-}
-
-function _getExistence(req, res) {
-
-  if (!req.query.data) {
-    res.send(JSON.stringify({data: []}));
-    return;
-  }
-
-  var result = [],
-      page,
-      n = req.query.data.length;
-
-  req.query.data.forEach(function(pageName, idx) {
-    (function(name, index) {
-      page = new models.Page(name);
-      if (!fs.existsSync(page.pathname)) {
-        result.push(name);
-      }
-      if (index == (n - 1)) {
-        res.send(JSON.stringify({data: result}));
-      }
-    })(pageName, idx);
   });
 }
 
